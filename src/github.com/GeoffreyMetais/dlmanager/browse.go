72ff600813ea0a01eb9fe09b8f07d135e66e37bc
Geoffrey Métais <geoffrey.metais@gmail.com>
2015-04-21 23:13:13 +0200

    Move persistency code in its own file

--- src/github.com/GeoffreyMetais/dlmanager/browse.go
+++ src/github.com/GeoffreyMetais/dlmanager/browse.go
@@ -98,27 +98,6 @@ func browseDir(w rest.ResponseWriter, req *rest.Request) {
 	}
 }
 
-func add(w rest.ResponseWriter, req *rest.Request) {
-    var newfile SharedFile
-    req.DecodeJsonPayload(&newfile)
-    if len(newfile.Path) > 0 && len(newfile.Name) > 0 {
-        fmt.Println("adding ", newfile.Name)
-        newfile.Link = settings.BaseUrl+"go/dl/"+newfile.Name
-        filesCollection.Pool[newfile.Name] = newfile
-        SaveCollection()
-//         w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.WriteHeader(http.StatusOK)
-    }
-}
-
-func remove(w rest.ResponseWriter, req *rest.Request) {
-    var filename = req.PathParam("name")
-    fmt.Println("removing ", filename)
-    delete(filesCollection.Pool, filename)
-    SaveCollection()
-    w.WriteHeader(http.StatusOK)
-}
-
 func download(w rest.ResponseWriter, req *rest.Request) {
     fmt.Println("Download ")
     var filename = req.PathParam("name")
@@ -156,35 +135,6 @@ func Init(){
         configFile.Close()
     }
     ReadCollection()
-}
-
-func ReadCollection() {
-    collectionFile, err := os.Open("collection.json")
-    if err != nil {
-        fmt.Println("opening collection file", err.Error())
-    } else {
-        var data = &filesCollection.Pool
-        jsonParser := json.NewDecoder(collectionFile)
-        if err = jsonParser.Decode(&data); err != nil {
-            fmt.Println("parsing collection file", err.Error())
-        }
-        collectionFile.Close()
-    }
-}
-
-func SaveCollection() {
-    collectionFile, err := os.Create("collection.json")
-    var data = &filesCollection.Pool
-    if err != nil {
-        fmt.Println("opening collection file", err.Error())
-    } else {
-         fmt.Println("collection file opened, writing data")
-        enc := json.NewEncoder(collectionFile)
-        if err = enc.Encode(data); err != nil {
-            fmt.Println("parsing collection file", err.Error())
-        }
-        collectionFile.Close()
-    }
 }
 
 func test() {
