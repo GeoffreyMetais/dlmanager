67a1fcf007195e9d15923246bd24a47256218bfd
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-06-24 15:23:03 +0200

    Update json format

--- src/github.com/GeoffreyMetais/dlmanager/browse.go
+++ src/github.com/GeoffreyMetais/dlmanager/browse.go
@@ -1,58 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/ant0ine/go-json-rest/rest"
 	"io/ioutil"
 	"net/http"
-        "encoding/json"
-	"strings"
 	"os"
-        "strconv"
+	"strconv"
+	"strings"
+
+	"github.com/ant0ine/go-json-rest/rest"
 )
 
 var settings struct {
-    Root string
-    Port string
-    BaseUrl string
+	Root    string
+	Port    string
+	BaseUrl string
 }
 
 var filesCollection struct {
-   Pool map[string]SharedFile 
+	Pool map[string]SharedFile `json:"sharesList"`
 }
 
 type SharedFile struct {
-    Name string
-    Path string
-    Link string
+	Name string `json:"name"`
+	Path string `json:"path"`
+	Link string `json:"link"`
 }
 
 type User struct {
-	Id   string
-	Name string
+	Id   string `json:"id"`
+	Name string `json:"name"`
 }
 
 type Directory struct {
-	Path  string
-	Files []File
+	Path  string `json:"path"`
+	Files []File `json:"files"`
 }
 
 type File struct {
-	Name  string
-	Path  string
-	IsDir bool
-	Size  int64
+	Name  string `json:"name"`
+	Path  string `json:"path"`
+	IsDir bool   `json:"isDirectory"`
+	Size  int64  `json:"size"`
 }
 
 type ReqBody struct {
-	Path string
-	User string
+	Path string `json:"path"`
+	User string `json:"user"`
 }
 
 func browseDir(w rest.ResponseWriter, req *rest.Request) {
 	//basePath := "/mnt/hdd/usb/"
 	//basePath := "/home/metais/Vidéos/"
-        basePath := settings.Root
+	basePath := settings.Root
 	var request ReqBody
 	req.DecodeJsonPayload(&request)
 	var path string
@@ -61,119 +62,123 @@ func browseDir(w rest.ResponseWriter, req *rest.Request) {
 	} else {
 		path = basePath + req.PathParam("dir")
 	}
-        if !strings.HasPrefix(path, basePath) {
-            rest.Error(w, "Permission denied", 503)
-            return
-        }
+	if !strings.HasPrefix(path, basePath) {
+		rest.Error(w, "Permission denied", 503)
+		return
+	}
 
 	fi, err := os.Stat(path)
-        if err != nil {
-            rest.NotFound(w, req)
-            return
-        }
+	if err != nil {
+		rest.NotFound(w, req)
+		return
+	}
 	if fi.IsDir() {
-            if !strings.HasSuffix(path, "/") {
-                    path = path + "/"
-            }
-            filesInfo, _ := ioutil.ReadDir(path)
-            //	files := [len(filesInfo)]string
-            files := make([]File, len(filesInfo))
-            for i := 0; i < len(filesInfo); i++ {
-                    files[i] = File{
-                            Name:  filesInfo[i].Name(),
-                            Path:  path + filesInfo[i].Name(),
-                            Size:  filesInfo[i].Size(),
-                            IsDir: filesInfo[i].IsDir(),
-                    }
-            }
-            dir := Directory{
-                    Path:  path,
-                    Files: files,
-            }
-            /*w.Header().Set("Access-Control-Allow-Origin", "*")
-            w.Header().Set("Access-Control-Allow-Methods", "POST")*/
-            w.WriteJson(&dir)
+		if !strings.HasSuffix(path, "/") {
+			path = path + "/"
+		}
+		filesInfo, _ := ioutil.ReadDir(path)
+		//	files := [len(filesInfo)]string
+		files := make([]File, len(filesInfo))
+		for i := 0; i < len(filesInfo); i++ {
+			files[i] = File{
+				Name:  filesInfo[i].Name(),
+				Path:  path + filesInfo[i].Name(),
+				Size:  filesInfo[i].Size(),
+				IsDir: filesInfo[i].IsDir(),
+			}
+		}
+		dir := Directory{
+			Path:  path,
+			Files: files,
+		}
+		/*w.Header().Set("Access-Control-Allow-Origin", "*")
+		  w.Header().Set("Access-Control-Allow-Methods", "POST")*/
+		w.WriteJson(&dir)
 	} else {
-                http.ServeFile(w.(http.ResponseWriter), req.Request, path)
+		http.ServeFile(w.(http.ResponseWriter), req.Request, path)
 	}
 }
 
 func download(w rest.ResponseWriter, req *rest.Request) {
-    fmt.Println("Download ")
-    var filename = req.PathParam("name")
-    fmt.Println("Download ", filename)
-    fmt.Println("path ", filesCollection.Pool[filename].Path)
-    fi, err := os.Stat(filesCollection.Pool[filename].Path)
-    if err != nil {
-        rest.NotFound(w, req)
-        return
-    }
-    fmt.Println("stats ", fi.Mode())
-    fmt.Println("stats name ", fi.Name())
-    fmt.Println("formatted size ", strconv.FormatInt(fi.Size(),10))
-    w.Header().Add("Content-type", "application/octet-stream");
- //   w.Header().Add("Content-Type", "application/force-download")
-    w.Header().Add("Content-Disposition", "attachment; filename="+fi.Name())
-    w.Header().Add("Content-length", strconv.FormatInt(fi.Size(),10))
-    http.ServeFile(w.(http.ResponseWriter), req.Request, filesCollection.Pool[filename].Path)
+	fmt.Println("Download ")
+	var filename = req.PathParam("name")
+	fmt.Println("Download ", filename)
+	fmt.Println("path ", filesCollection.Pool[filename].Path)
+	fi, err := os.Stat(filesCollection.Pool[filename].Path)
+	if err != nil {
+		rest.NotFound(w, req)
+		return
+	}
+	fmt.Println("stats ", fi.Mode())
+	fmt.Println("stats name ", fi.Name())
+	fmt.Println("formatted size ", strconv.FormatInt(fi.Size(), 10))
+	w.Header().Add("Content-type", "application/octet-stream")
+	//   w.Header().Add("Content-Type", "application/force-download")
+	w.Header().Add("Content-Disposition", "attachment; filename="+fi.Name())
+	w.Header().Add("Content-length", strconv.FormatInt(fi.Size(), 10))
+	http.ServeFile(w.(http.ResponseWriter), req.Request, filesCollection.Pool[filename].Path)
 }
 
 func listShares(w rest.ResponseWriter, req *rest.Request) {
-//     w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.WriteJson(&filesCollection.Pool)
+	//     w.Header().Set("Access-Control-Allow-Origin", "*")
+	list := make([]SharedFile, 0, len(filesCollection.Pool))
+	for _, value := range filesCollection.Pool {
+		list = append(list, value)
+	}
+	w.WriteJson(list)
 }
 
-func init(){
-    configFile, err := os.Open("config.json")
-    if err != nil {
-        fmt.Println("opening config file", err.Error())
-    } else {
-        defer configFile.Close();
-        jsonParser := json.NewDecoder(configFile)
-        if err = jsonParser.Decode(&settings); err != nil {
-            fmt.Println("parsing config file", err.Error())
-        }
-    }
-    ReadCollection()
+func init() {
+	configFile, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println("opening config file", err.Error())
+	} else {
+		defer configFile.Close()
+		jsonParser := json.NewDecoder(configFile)
+		if err = jsonParser.Decode(&settings); err != nil {
+			fmt.Println("parsing config file", err.Error())
+		}
+	}
+	ReadCollection()
 }
 
 func test() {
-//     ReadCollection()
-    for filename := range filesCollection.Pool {
-        fmt.Println("name", filename)
-        fmt.Println("path", filesCollection.Pool[filename].Path)
-        fmt.Println("Link", filesCollection.Pool[filename].Link)
-    }
-//     filesCollection.Pool["troisème"] = SharedFile{"DBZ","/home/metais/Vidéos/[DB-Z.com] Dragon Ball Z Battle of Gods [720p][VOSTFR].mp4"}
-//     SaveCollection()
-//     fmt.Println("Collection saved")
+	//     ReadCollection()
+	for filename := range filesCollection.Pool {
+		fmt.Println("name", filename)
+		fmt.Println("path", filesCollection.Pool[filename].Path)
+		fmt.Println("Link", filesCollection.Pool[filename].Link)
+	}
+	//     filesCollection.Pool["troisème"] = SharedFile{"DBZ","/home/metais/Vidéos/[DB-Z.com] Dragon Ball Z Battle of Gods [720p][VOSTFR].mp4"}
+	//     SaveCollection()
+	//     fmt.Println("Collection saved")
 }
 
 func main() {
 	handler := rest.ResourceHandler{
-            PreRoutingMiddlewares: []rest.Middleware{
-                        &rest.CorsMiddleware{
-                                RejectNonCorsRequests: false,
-                                OriginValidator: func(origin string, request *rest.Request) bool {
-                                        return true //origin == "http://localhost:8000"
-                                },
-                                AllowedMethods: []string{"GET", "POST", "PUT"},
-                                AllowedHeaders: []string{
-                                        "Accept", "Content-Type", "X-Custom-Header", "Origin"},
-                                AccessControlAllowCredentials: true,
-                                AccessControlMaxAge:           3600,
-                        },
-                },
-        }
+		PreRoutingMiddlewares: []rest.Middleware{
+			&rest.CorsMiddleware{
+				RejectNonCorsRequests: false,
+				OriginValidator: func(origin string, request *rest.Request) bool {
+					return true //origin == "http://localhost:8000"
+				},
+				AllowedMethods: []string{"GET", "POST", "PUT"},
+				AllowedHeaders: []string{
+					"Accept", "Content-Type", "X-Custom-Header", "Origin"},
+				AccessControlAllowCredentials: true,
+				AccessControlMaxAge:           3600,
+			},
+		},
+	}
 	handler.SetRoutes(
-		&rest.Route{"GET",  "/go/browse/:dir", browseDir},
+		&rest.Route{"GET", "/go/browse/:dir", browseDir},
 		&rest.Route{"POST", "/go/browse", browseDir},
-                &rest.Route{"GET",  "/go/browse", browseDir},
-                &rest.Route{"GET",  "/go/dl/:name", download},
-                &rest.Route{"POST", "/go/add", add},
-                &rest.Route{"DELETE",  "/go/del/:name", remove},
-                &rest.Route{"GET",  "/go/list", listShares},
+		&rest.Route{"GET", "/go/browse", browseDir},
+		&rest.Route{"GET", "/go/dl/:name", download},
+		&rest.Route{"POST", "/go/add", add},
+		&rest.Route{"DELETE", "/go/del/:name", remove},
+		&rest.Route{"GET", "/go/list", listShares},
 	)
-        //http.HandleFunc("/dl", download)
+	//http.HandleFunc("/dl", download)
 	http.ListenAndServe(settings.Port, &handler)
 }
